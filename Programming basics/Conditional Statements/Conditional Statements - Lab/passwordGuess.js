8995a034e83375d94a63455b7695e177ad27541c
steliyanadim <steliyanadim@gmail.com>
2022-07-25 13:02:02 +0300

    02. Exercise: Conditional Statements

---
+++ Programming basics/Conditional Statements/Conditional Statements - Lab/passwordGuess.js
@@ -0,0 +1,11 @@
+function passwordGuess(input){
+let password = (input[0]);
+let correctPassword = "s3cr3t!P@ssw0rd"
+
+if (password === correctPassword){
+    console.log("Welcome");
+} else {
+    console.log("Wrong password!");
+}
+}
+passwordGuess(["s3cr3t!P@ssw0rd"])
