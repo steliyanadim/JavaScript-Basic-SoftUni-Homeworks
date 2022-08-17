54410b0d0b5a1daef34a69647d60c8376ecdd85f
steliyanadim <steliyanadim@gmail.com>
2022-08-17 13:25:45 +0300

    03. Exercise: Conditional Statements Advanced

---
+++ Conditional Statements Advanced/Conditional Statements Advanced - Lab/workingHours.js
@@ -0,0 +1,18 @@
+function workingHours(input) {
+    let hour = Number(input[0]);
+    let day = input[1];
+
+    if (hour >= 10 && hour < 18) {
+        switch (day) {
+            case 'Monday':
+            case 'Tuesday':
+            case 'Wednesday':
+            case 'Thursday':
+            case 'Friday':
+            case 'Saturday': console.log('open'); break;
+        }
+    } else {
+        console.log('closed');
+    }
+}
+workingHours(["17.3", "Monday"])
