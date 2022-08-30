f0073b12abcd2c3d80d4747ae5f5bcb4ac97e681
steliyanadim <steliyanadim@gmail.com>
2022-08-30 18:41:53 +0300

    04. Exercise: For Loop

---
+++ For Loop/For Loop - Lab/characterSequence.js
@@ -0,0 +1,14 @@
+function characterSequence (input){
+let text = input[0];
+
+for (let i =0; i < text.length; i++){
+
+
+}
+
+if (text[5] < 6){
+    console.log(text[5])  
+}
+
+}
+characterSequence(["softuni"])
